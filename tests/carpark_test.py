ab05e674b43dd1ebcddfc923cfa029075833a588
Jason H <20134959@tafe.wa.edu.au>
2025-11-27 06:01:09 +0800

    refactor(rewrote): rewrote the test code to fit my code

--- tests/carpark_test.py
+++ tests/carpark_test.py
@@ -9,11 +9,35 @@ sys.path.append(parent + "/smartpark")
 #Change the line below to import your manager class
 from carpark_manager import CarparkManager
 
-class TestConfigParsing(unittest.TestCase):
+class TestCarparkManager(unittest.TestCase):
+    """ 
+    Testing class for CarparkManager Behaviour.
+    """
+    def test_carpark_has_spaces(self):
+        """
+        This should test a fresh carpark having all avialable spaces.
+        """
+        manager = CarparkManager(location = 'Test Lot', total_space = 1000)
+        self.assertEqual(1000, manager.available_space)
+        
+    def test_enter_exit_car(self):
+        """
+        This should test entering and exiting cars in which should adjust available spaces correctly.
+        """
+        manager = CarparkManager(location = 'Test Lot', total_space = 10)
+       
+        manager.car_enter('TJN-449')
+        self.assertEqual(9, manager.available_spaces)
+        
+        manager.car_enter('SKK-078')
+        self.assertEqual(8, manager.available_spaces)
+       
+        manager.car_enter('NXI-228')
+        self.assertEqual(7, manager.available_spaces)
+        
+        manager.car_exit('TJN-449')
+        self.assertEqual(8, manager.available_spaces)
 
-    def test_fresh_carpark(self):
-        carpark = CarparkManager(location = 'Test', total_space = 1000)
-        self.assertEqual(1000,carpark.available_spaces)
-
-if __name__=="__carpark_main__":
+if __name__ == '__main__':
     unittest.main()
+        
