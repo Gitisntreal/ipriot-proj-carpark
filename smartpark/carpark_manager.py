3f93d014713a03b3d42271ee069710fa570c8373
Jason H <20134959@tafe.wa.edu.au>
2025-11-27 05:58:33 +0800

    fix/feat(indenting): forgot to indent all the def variable into the class and also added temperature reading cause i forgot to after adjustments

--- smartpark/carpark_manager.py
+++ smartpark/carpark_manager.py
@@ -18,43 +18,78 @@ class CarparkManager(CarparkSensorListener, CarparkDataProvider):
 
     """
 
-def __init__(self, location: str, total_space: int):
-    self._location = location
-    self._total_space = total_space
-    self._temperature = 0.0
-    self._cars_in_park = 0
+    def __init__(self, location: str, total_space: int) -> None:
+        """ Create a new carpark manager.
 
-# Implementing CarparkSensorLinstener 
+        Args:
+        location (str):  location of the carpark.
+        total_space (int): The total number of spaces.
+        
+    """
+        self._location = location
+        self._total_space = total_space
+        self._temperature = 0.0
+        self._cars_in_park = 0
 
-def car_enter(self, temperature: float):
-    """ calls when a car enter the car park"""
-    self._cars_in_park += 1
-    self._temperature = temperature
-    logging.info('Car has entered. Cars in park: %s, temp: %s',
-                 self._cars_in_park, self._temperature)
     
-def car_exit(self, temperature: float):
-    """ calls when a car exit the carpark"""
-    if self._cars_in_park > 0:
-        self._cars_in_park -= 1 
-    self._temperature = temperature
-    logging.info('Car has exited. Cars in park: %s, temp: %s',
-                 self._cars_in_park, self._temperature)
+        logging.info(
+            'CarparkManager created for "%s" with %s total space',
+            self.location,
+            self._total_space,
+        )
+
+    # Implementing CarparkSensorLinstener 
+
+    def car_enter(self, liscense_plate: str) -> None:
+        """
+        Senses and signals when a car has parked.
+        """
+        self._cars_in_park += 1
+        if liscense_plate:
+            self._cars.add(liscense_plate)
+        
+        logging.info(
+            'Car entering: %s | cars in park: %s',
+            liscense_plate,
+            self._cars_in_park,
+        )
+    
+    def car_exit(self, liscense_plate: str) -> None:
+        """
+        Senses and signals when a car has left the space.
+        """
+        if self._cars_in_park > 0:
+            self._cars_in_park -= 1 
+        if liscense_plate and liscense_plate in self._cars:
+            self._cars.remove(liscense_plate)
+        
+        logging.info(
+            'Car exiting: %s | cars in park: %s',
+            liscense_plate,
+            self._cars_in_park,
+        )
+    
+    def temperature(self, reading: float) -> None:
+        """
+        Provides the temperature reading.
+        """
+        self._temperature = float(reading)
+        logging.info('Temperature updated: %.2f', self._temperature)
 
 # Implementing CarperkDataProvider 
 
-@property
-def available_spaces(self) -> int:
-    """ should show the amount of space left (if it in negative something is wrong)."""
-    space_available = self._total_space - self._cars_in_park
-    return max(space_available, 0)
+    @property
+    def available_spaces(self) -> int:
+        """ should show the amount of space left (if it in negative something is wrong)."""
+        space_available = self._total_space - self._cars_in_park
+        return max(space_available, 0)
 
-@property
-def temperature(self) -> float:
-    """ should show the temperature"""
-    return self._temperature
+    @property
+    def temperature(self) -> float:
+        """ should show the current temperature"""
+        return self._temperature
 
-@property
-def current_time(self) -> str:
-    """ Should show the current time as a string (allows to use of OCT/NOV/ DEC ect) in display"""
-    return dt.now().strftime('%H:%M:%S')
+    @property
+    def current_time(self) -> str:
+        """ Should show the current time as HH:MM:SS in display"""
+        return dt.now().strftime('%H:%M:%S')
