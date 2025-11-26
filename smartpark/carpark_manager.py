63b90c2fb5540aabf37b8c39b0d27469fc5cb44a
Jason H <20134959@tafe.wa.edu.au>
2025-11-27 03:50:40 +0800

    feat(carpark_manager): implements cars enetering/exiting, tracks avialability of spaces, tracks the temp and time.

---
+++ smartpark/carpark_manager.py
@@ -0,0 +1,49 @@
+from datetime import datetime as dt
+from smartpark.interfaces import CarparkSensorListener, CarparkDataProvider
+
+class CarparkManager(CarparkSensorListener, CarparkDataProvider):
+    """
+
+    Carpark management state: where cars enters/exits, space aviability, temperature and time.
+    implenmentation of both:
+        - CarparkSensorListener (reacts to sensor events)
+        - CarparkDataProvider (provide data to display)
+
+    """
+
+def __init__(self, location: str, total_space: int):
+    self._location = location
+    self._total_space = total_space
+    self._temperature = 0.0
+    self._cars_in_park = 0
+
+# Implementing CarparkSensorLinstener 
+
+def car_enter(self, temperature: float):
+    """ calls when a car enter the car park"""
+    self._cars_in_park += 1
+    self._temperature = temperature
+    
+def car_exit(self, temperature: float):
+    """ calls when a car exit the carpark"""
+    if self._cars_in_park > 0:
+        self._cars_in_park -= 1 
+    self._temperature = temperature
+
+# Implementing CarperkDataProvider 
+
+@property
+def available_spaces(self) -> int:
+    """ should show the amount of space left (if it in negative something is wrong)."""
+    space_available = self._total_space - self._cars_in_park
+    return max(space_available, 0)
+
+@property
+def temperature(self) -> float:
+    """ should show the temperature"""
+    return self._temperature
+
+@property
+def current_time(self) -> str:
+    """ Should show the current time as a string (allows to use of OCT/NOV/ DEC ect) in display"""
+    return datetime.now().strftime('%H:%M:%S')
